1f0815affc3b03cf94618f7502a6536ad4c9fd0b
filip7557 <filip.curic@student.ferit.hr>
2024-10-04 14:24:38 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Dish;
 
 Route::get('/', function () {
-    return view('welcome');
+    //get filtered ones and return
+    return Dish::all();
+});
+
+Route::get('/greeting', function () {
+    return 'Hello World!';
 });
