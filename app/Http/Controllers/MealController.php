3a5714e9828dabe4031be2b38ca1d0130549fe92
filip7557 <filip.curic@student.ferit.hr>
2024-10-07 03:43:03 +0200

    Add response to index request

--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -11,21 +11,6 @@ class MealController extends Controller
     public function index()
     {
         $meals = Meal::all();
-        $new_meals = array_map(function ($meal) {
-            $new_meal = new Meal;
-            $new_meal->id = $meal->id;
-            $new_meal->title = $meal->title;
-            $new_meal->description = $meal->description;
-            if ($meal->category == 0)
-                $new_meal->category = NULL;
-            else
-                $new_meal->category = Category::where('id', $meal->category)->firstorfail();
-            if ($meal->updated_at != $meal->created_at)
-                $new_meal->status = "modified";
-            else
-                $new_meal->status = "created";
-            return $new_meal;
-        }, json_decode(json_encode($meals)));
-        return response() -> json(['data' => $new_meals]);
+        return response() -> json(['data' => Meal::to_json($meals)]);
     }
 }
